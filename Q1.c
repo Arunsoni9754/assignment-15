9077eb7fd22810726e3f358f3b41510eec08c0c4
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-14 22:12:42 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+//int large(int);
+int large(int a[])
+{
+    int i;
+    for(i=1;i<=9;i++)
+    {
+        if(a[0]<a[i])
+        {
+            a[0]=a[i];    
+        }
+    }
+    return a[0];
+}
+int main()
+{
+   int a[10];
+    int i;
+    printf("Enter the values ");
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int j=large(a);
+        printf("The largest number is %d",j);
+
+return 0;
+}
